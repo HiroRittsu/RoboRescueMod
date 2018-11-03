1ea76722e61a05eea2be7c2537279972a5332fe1
MiglyA <zq6295migly@gmail.com>
2018-11-03 15:33:16 +0900

    [update]calcPrimaryPoint関数作成

--- src/main/java/com/roborescuemod/commons/PointConverter.java
+++ src/main/java/com/roborescuemod/commons/PointConverter.java
@@ -6,17 +6,34 @@ public class PointConverter {
 
 	public void calcPrimaryPoint(Map<Integer, Point3D> nodes, Point3Df max, Point3Df min, Point3Df centroid) {
 
-		max.x = Double.MIN_VALUE;
-		max.y = Double.MIN_VALUE;
-		max.z = Double.MIN_VALUE;
-		
+		max.resetPoint("MIN");
+		min.resetPoint("MAX");
+
 		for (Point3D p : nodes.values()) {
 
-			if (p.getX() > min.getX()) {
-				
-			}
+			if (min.getX() > p.getX())
+				min.x = p.getX();
+
+			if (min.getY() > p.getY())
+				min.y = p.getY();
+
+			if (min.getZ() > p.getZ())
+				min.z = p.getZ();
+
+			if (max.getX() < p.getX())
+				max.x = p.getX();
+
+			if (max.getY() < p.getY())
+				max.y = p.getY();
+
+			if (max.getZ() < p.getZ())
+				max.z = p.getZ();
 
 		}
+
+		centroid.x = (max.getX() + min.getX()) / 2.0;
+		centroid.y = (max.getY() + min.getY()) / 2.0;
+		centroid.z = (max.getZ() + min.getZ()) / 2.0;
 
 	}
 }
