2670395dbcc33380857f03d5a957def8e6e450dc
MiglyA <zq6295migly@gmail.com>
2018-11-03 15:10:42 +0900

    [update]マップ要素の取得方法を変更

--- src/main/java/com/roborescuemod/buildmap/MinecraftMap.java
+++ src/main/java/com/roborescuemod/buildmap/MinecraftMap.java
@@ -5,20 +5,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.roborescuemod.commons.Point3D;
+import com.roborescuemod.commons.PointConverter;
 
 public class MinecraftMap {
 
-	public Map<Integer, Point3D> nodes = new HashMap<>();
-	public Map<Integer, Edge> edges = new HashMap<>();
-	public ArrayList<Road> roads = new ArrayList<>();
-	public ArrayList<Building> buildings = new ArrayList<>();
+	private Map<Integer, Point3D> nodes = null;
+	private Map<Integer, Edge> edges = null;
+	private ArrayList<Road> roads = null;
+	private ArrayList<Building> buildings = null;
+	private PointConverter converter = new PointConverter();
 
-	public MinecraftMap() {
-		
+	public MinecraftMap(Map<Integer, Point3D> nodes) {
+		for (Point3D p : nodes.values()) {
+
+		}
+	}
+
+	public void setNodes(Map<Integer, Point3D> nodes) {
+		this.nodes = nodes;
+	}
+
+	public Map<Integer, Point3D> getNodes() {
+		return this.nodes;
+	}
+
+	public void setEdges(Map<Integer, Edge> edges) {
+		this.edges = edges;
+	}
+
+	public Map<Integer, Edge> getEdges() {
+		return this.edges;
+	}
+
+	public void setRoads(ArrayList<Road> roads) {
+		this.roads = roads;
+	}
+
+	public ArrayList<Road> getRoads() {
+		return this.roads;
+	}
+
+	public void setBuildings(ArrayList<Building> buildings) {
+		this.buildings = buildings;
+	}
+
+	public ArrayList<Building> getBuildins() {
+		return this.buildings;
 	}
 
 	public Point3D getCentroid() {
-		
+
 		return null;
 	}
 
--- src/main/java/com/roborescuemod/commons/PointConverter.java
+++ src/main/java/com/roborescuemod/commons/PointConverter.java
@@ -1,5 +1,12 @@
 package com.roborescuemod.commons;
 
+import java.util.Map;
+
 public class PointConverter {
 
+	public Point3D calcCentroid(Map<Integer, Point3D> nodes) {
+		
+		
+		return null;
+	}
 }
