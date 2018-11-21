2357ba1ffd2282a4baca55ccf2e4ca4d9e85bb13
MiglyA <zq6295migly@gmail.com>
2018-11-21 15:44:40 +0900

    [update]Renderクラスにマップ構築用関数作成

--- src/main/java/com/roborescue/information/Worldinfo.java
+++ src/main/java/com/roborescue/information/Worldinfo.java
@@ -1,5 +1,25 @@
 package com.roborescue.information;
 
+import com.roborescue.world.map.GMLMap;
+import com.roborescue.world.map.MinecraftMap;
+import com.roborescue.world.map.RescueMap;
+
 public class Worldinfo {
+
+	public static MinecraftMap minecraftMap;
+	public static RescueMap rescueMap;
+	public static GMLMap gmlMap;
+
+	public static boolean canMinecraftMap() {
+		return false;
+	}
+
+	public static boolean canRescueMap() {
+		return false;
+	}
+
+	public static boolean canGMLMap() {
+		return false;
+	}
 
 }
--- src/main/java/com/roborescue/render/Render.java
+++ src/main/java/com/roborescue/render/Render.java
@@ -1,17 +1,46 @@
 package com.roborescue.render;
 
+import com.roborescue.information.Worldinfo;
+import com.roborescue.render.map.SetMap;
+
 import net.minecraft.world.World;
 
 public class Render {
 
 	public World world;
+	public SetMap setMap;
+	private int build_index = 0;
+	private int road_index = 0;
+	public boolean readyMap = false;
 
 	public Render(World world) {
 		this.world = world;
+		this.setMap = new SetMap();
 	}
 
 	public void renderMap() {
-		
+		System.out.println("renderMap");
+
+		if (Worldinfo.canMinecraftMap()) {
+
+			// Road
+			if (road_index != -1 && !setMap.drawRoad(road_index, Worldinfo.minecraftMap, world)) {
+				road_index = -1;
+			} else {
+				road_index++;
+			}
+
+			// Building
+			if (build_index != -1 && setMap.drawBuildings(build_index, Worldinfo.minecraftMap, world)) {
+				build_index = -1;
+			} else {
+				build_index++;
+			}
+
+			if (road_index == -1 && build_index == -1) {
+				readyMap = true;
+			}
+		}
 	}
 
 	public void actionAgent() {
