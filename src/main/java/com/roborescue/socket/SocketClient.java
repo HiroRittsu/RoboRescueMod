957fa3b39c7c3d7f894cbda3ef3503d8a18db929
MiglyA <zq6295migly@gmail.com>
2018-11-21 16:00:25 +0900

    [add]socket関係のクラス追加

---
+++ src/main/java/com/roborescue/socket/SocketClient.java
@@ -0,0 +1,30 @@
+package com.roborescue.socket;
+
+public class SocketClient {
+
+	private OriginalSocket originalSocket = new OriginalSocket();
+
+	public SocketClient() {
+		threadSocket();
+	}
+
+	private void threadSocket() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+
+				originalSocket.joinClient(12345, "localhost");
+
+				while (true) {
+
+					//RescueInfo.registerData(originalSocket.subscribeMsgs());
+
+					originalSocket.delaitinon(1);
+
+				}
+
+			}
+		}).start();
+	}
+
+}
---
+++ src/main/java/com/roborescue/socket/SocketReader.java
@@ -0,0 +1,5 @@
+package com.roborescue.socket;
+
+public class SocketReader {
+
+}
