9803855a7cbf7c345ab0556ed3143a5e1258643d
MiglyA <zq6295migly@gmail.com>
2018-11-21 11:05:21 +0900

    [update]RoboRescueModメインクラス更新

---
+++ src/main/java/com/roborescue/roborescuemod/RoboRescueMod.java
@@ -0,0 +1,38 @@
+package com.roborescue.roborescuemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = RoboRescueMod.MODID, name = RoboRescueMod.NAME, version = RoboRescueMod.VERSION)
+public class RoboRescueMod {
+	public static final String MODID = "roborescuemod";
+	public static final String NAME = "RoboRescue Mod";
+	public static final String VERSION = "2.0";
+
+	private static Logger logger;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(this);
+		logger = event.getModLog();
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// some example code
+		logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	}
+	
+	@SubscribeEvent
+	public void ServerTick(TickEvent.ServerTickEvent event) {
+		System.out.println("ServerTick");
+	}
+}
