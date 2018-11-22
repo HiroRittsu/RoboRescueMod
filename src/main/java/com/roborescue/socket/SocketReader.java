155fa5300bdbd0364b63318f2f5efc541186d929
MiglyA <zq6295migly@gmail.com>
2018-11-22 11:21:09 +0900

    [update]buildingデータ受け取り関数作成

--- src/main/java/com/roborescue/socket/SocketReader.java
+++ src/main/java/com/roborescue/socket/SocketReader.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.roborescue.commons.Point3D;
+import com.roborescue.world.map.parts.Building;
 import com.roborescue.world.map.parts.Edge;
 import com.roborescue.world.map.parts.Road;
 
@@ -17,9 +18,10 @@ public class SocketReader {
 		// {entityID, x, y, z}
 		Map<Integer, Point3D> node = new HashMap<>();
 		for (int i = 1; i < msgs.length; i += 4) {
+			int id = Integer.parseInt(msgs[i]);
 			Point3D point3d = new Point3D((int) Double.parseDouble(msgs[i + 1]), (int) Double.parseDouble(msgs[i + 2]),
 					(int) Double.parseDouble(msgs[i + 3]));
-			node.put(Integer.parseInt(msgs[i]), point3d);
+			node.put(id, point3d);
 		}
 	}
 
@@ -28,29 +30,37 @@ public class SocketReader {
 		// {entityID, firstID, endID}
 		Map<Integer, Edge> edges = new HashMap<>();
 		for (int i = 1; i < msgs.length; i += 3) {
+			int id = Integer.parseInt(msgs[i]);
 			Integer[] nodes = new Integer[2];
 			nodes[0] = Integer.parseInt(msgs[i + 1]);
 			nodes[1] = Integer.parseInt(msgs[i + 2]);
-			Edge edge = new Edge(Integer.parseInt(msgs[i]), nodes);
-			edges.put(Integer.parseInt(msgs[i]), edge);
+			Edge edge = new Edge(id, nodes);
+			edges.put(edge.getId(), edge);
 		}
 	}
 
 	public void readRoad(String msg) {
 		String[] msgs = msg.split(",");
 		// {entityID, edgeID,・・・ }
+		int id = Integer.parseInt(msgs[0]);
 		ArrayList<Integer> edge_ids = new ArrayList<>();
 		for (int i = 1; i < msgs.length; i++) {
 			edge_ids.add(Integer.parseInt(msgs[i]));
 		}
-		Road road = new Road(Integer.parseInt(msgs[0]), edge_ids);
+		Road road = new Road(id, edge_ids);
 	}
 
 	public void readBuilding(String msg) {
-		// {entityID, edgeID,・・・ }
-		for (int i = 1; i < msg.split(",").length; i++) {
-
+		String[] msgs = msg.split(",");
+		// {entityID, floor, material, edgeID,・・・ }
+		int id = Integer.parseInt(msgs[0]);
+		int floor = Integer.parseInt(msgs[1]);
+		String material = msgs[2];
+		ArrayList<Integer> edge_ids = new ArrayList<>();
+		for (int i = 3; i < msgs.length; i++) {
+			edge_ids.add(Integer.parseInt(msgs[i]));
 		}
+		Building building = new Building(id, floor, material, edge_ids);
 	}
 
 	////////////////////////////////////////////
